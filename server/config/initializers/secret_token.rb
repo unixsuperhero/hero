8cae7bb0abd191dfdd627054dc146ef74fe9288a
Joshua Toyota <jearsh@gmail.com>
2015-03-06 09:17:11 -0500

    add rails app: Hero Server to (hero)/server

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeroServer::Application.config.secret_key_base = '3b46ad8c7e2df76c28a6cf7b4fb1c1896806dac938043a1c39cd33704881abaf08fdf25ddc1dde6054e98cfd539f48291ed6b1cc8e22bcdc7f95f6a8bb30e51c'
---
+++ server/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HeroServer::Application.config.session_store :cookie_store, key: '_hero_server_session'
