d522159579b790a956783746a346956ee370fb70
Joshua Toyota <jearsh@gmail.com>
2015-04-06 20:25:38 -0400

    autocommitting base_status_value_object_pattern.rb

--- experiments/ruby/base_status_value_object_pattern.rb
+++ experiments/ruby/base_status_value_object_pattern.rb
@@ -146,6 +146,14 @@ class BaseStatusTwo
   }.to_mash
 
   class << self
+    def mapping # !> method redefined; discarding old mapping
+      @@mapping
+    end
+
+    def mapping_two
+      @@mapping_two
+    end
+
     def ids
       @@ids ||= @@mapping.keys
     end
@@ -205,7 +213,7 @@ end
 class CarStatusTwo < BaseStatusTwo
   class << self
     attr_accessor :imported, :walked, :worked, :completed, :hidden
-    attr_accessor :mapping_two
+    attr_accessor :mapping, :mapping_two
   end
 
   IMPORTED  = 0
@@ -214,13 +222,13 @@ class CarStatusTwo < BaseStatusTwo
   COMPLETED = 3
   HIDDEN    = 4
 
-  @@imported  = IMPORTED
-  @@walked    = WALKED
-  @@worked    = WORKED
-  @@completed = COMPLETED
-  @@hidden    = HIDDEN
+  @imported  = IMPORTED
+  @walked    = WALKED
+  @worked    = WORKED
+  @completed = COMPLETED
+  @hidden    = HIDDEN
 
-  @@mapping = {
+  @mapping = {
     IMPORTED  => 'imported',
     WALKED    => 'walked',
     WORKED    => 'worked',
@@ -228,12 +236,12 @@ class CarStatusTwo < BaseStatusTwo
     HIDDEN    => 'hidden',
   }.to_mash
 
-  @@mapping_two = {
-    @@imported  => 'imported',
-    @@walked    => 'walked',
-    @@worked    => 'worked',
-    @@completed => 'completed',
-    @@hidden    => 'hidden',
+  @mapping_two = {
+    @imported  => 'imported',
+    @walked    => 'walked',
+    @worked    => 'worked',
+    @completed => 'completed',
+    @hidden    => 'hidden',
   }.to_mash
 
   def imported?
@@ -267,6 +275,37 @@ end
 # if doing it that way, the include BaseStatus line probably
 # needs to be at the bottom, after the constants are defined
 
+module StatusObject
+  @mapping = {}
+  class << self
+    attr_reader :mapping
+  end
+
+  def self.extended(base)
+    base.constants.each do |c|
+      val = base.const_get(c)
+
+      base.instance_eval <<-INST
+        @#{c.downcase} = #{val}
+        @mapping.merge!(@#{c.downcase} => #{val})
+
+        class << self
+          attr_reader :#{c.downcase}
+        end
+      INST
+    end
+  end
+
+  @ids = @mapping.keys
+  @names = @mapping.values
+
+  @id_map = @mapping.invert.to_mash
+  @name_map = @mapping
+  class << self
+    attr_reader :ids, :names, :id_map, :name_map
+  end
+end
+
 class TestAutoGeneratingEverything
   IMPORTED  = 0
   WALKED    = 1
@@ -274,19 +313,50 @@ class TestAutoGeneratingEverything
   COMPLETED = 3
   HIDDEN    = 4
 
-  @mapping = Mash.new
-  constants.each do |c|
-    ap name: name, constant_name: c
-  end
+  # @mapping = Mash.new
+  # constants.each do |c|
+  #   ap name: name, constant_name: c
+  # end
+  extend StatusObject
 end
 
 
-CarStatus.ids # => nil
-CarStatus.names # => nil
-CarStatus.mapping # => nil
+# CarStatus.ids # => nil
+# CarStatus.names # => nil
+# CarStatus.mapping # => nil
+
+# [0,1,2,3,4].map do |id|
+#   cs = CarStatus.by_id(id) # => #<CarStatus:0x007fd299933c08 @id=0, @name=nil>, #<CarStatus:0x007fd299931750 @id=1, @name=nil>, #<CarStatus:0x007fd29992a748 @id=2, @name=nil>, #<CarStatus:0x007fd2999039b8 @id=3, @name=nil>, #<CarStatus:0x007fd299902360 @id=4, @name=nil>
+#   cs.id # => 0, 1, 2, 3, 4
+#   cs.name # => nil, nil, nil, nil, nil
+#   cs.display # => "", "", "", "", ""
+#   {
+#     imported?: cs.imported?,
+#     walked?: cs.walked?,
+#     worked?: cs.worked?,
+#     completed?: cs.completed?,
+#     hidden?: cs.hidden?
+#   }
+# end
+
+
+CarStatusTwo.ids # => ["0", "1"]
+CarStatusTwo.names # => ["example_status_a", "example_status_b"]
+CarStatusTwo.mapping # => <Mash 0="imported" 1="walked" 2="worked" 3="completed" 4="hidden">
+CarStatusTwo.mapping_two # => <Mash 0="imported" 1="walked" 2="worked" 3="completed" 4="hidden">
+CarStatusTwo.name_map # => <Mash 0="example_status_a" 1="example_status_b">
+CarStatusTwo.name_map_two # => <Mash 0="example_status_a" 1="example_status_b">
+CarStatusTwo.imported # => 0
+CarStatusTwo.walked # => 1
+CarStatusTwo.worked # => 2
+CarStatusTwo.completed # => 3
+CarStatusTwo.hidden # => 4
+CarStatusTwo.names # => ["example_status_a", "example_status_b"]
+CarStatusTwo.mapping # => <Mash 0="imported" 1="walked" 2="worked" 3="completed" 4="hidden">
+CarStatusTwo.mapping_two # => <Mash 0="imported" 1="walked" 2="worked" 3="completed" 4="hidden">
 
 [0,1,2,3,4].map do |id|
-  cs = CarStatus.by_id(id) # => #<CarStatus:0x007f8189bac450 @id=0, @name=nil>, #<CarStatus:0x007f8189b9f390 @id=1, @name=nil>, #<CarStatus:0x007f8189b9de28 @id=2, @name=nil>, #<CarStatus:0x007f8189b9cd70 @id=3, @name=nil>, #<CarStatus:0x007f8189b9e7b0 @id=4, @name=nil>
+  cs = CarStatusTwo.by_id(id) # => #<CarStatusTwo:0x007fd2998f8928 @id=0, @name=nil>, #<CarStatusTwo:0x007fd2998f33d8 @id=1, @name=nil>, #<CarStatusTwo:0x007fd2998f1d08 @id=2, @name=nil>, #<CarStatusTwo:0x007fd2998f0728 @id=3, @name=nil>, #<CarStatusTwo:0x007fd2998eec48 @id=4, @name=nil>
   cs.id # => 0, 1, 2, 3, 4
   cs.name # => nil, nil, nil, nil, nil
   cs.display # => "", "", "", "", ""
@@ -299,30 +369,14 @@ CarStatus.mapping # => nil
   }
 end
 
-
-CarStatusTwo.ids # => ["0", "1", "2", "3", "4"]
-CarStatusTwo.names # => ["imported", "walked", "worked", "completed", "hidden"]
-CarStatusTwo.name_map # => <Mash 0="imported" 1="walked" 2="worked" 3="completed" 4="hidden">
-CarStatusTwo.name_map_two # =>
-
-[0,1,2,3,4].map do |id|
-  cs = CarStatusTwo.by_id(id) # =>
-  cs.id # =>
-  cs.name # =>
-  cs.display # =>
-  {
-    imported?: cs.imported?,
-    walked?: cs.walked?,
-    worked?: cs.worked?,
-    completed?: cs.completed?,
-    hidden?: cs.hidden?
-  }
-end
-
 TestAutoGeneratingEverything::WALKED # =>
+TestAutoGeneratingEverything.walked # =>
+TestAutoGeneratingEverything.ids # =>
+TestAutoGeneratingEverything.names # =>
+TestAutoGeneratingEverything.id_map # =>
+TestAutoGeneratingEverything.name_map # =>
+TestAutoGeneratingEverything.mapping # =>
 
-# ~> -:161:in `name_map_two': uninitialized class variable @@mapping_two in BaseStatusTwo (NameError)
-# ~> 	from -:301:in `<main>'
 # >> {
 # >>              :name => "TestAutoGeneratingEverything",
 # >>     :constant_name => :IMPORTED
